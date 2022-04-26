e1a414db0adb87b20cc4ebd8213622255c130675
Frank Neblung <frank.neblung@gmail.com>
2022-04-26 16:04:49 +0200

    Comment violation of MandatoryBracesLoops
    Makes the build pass again.
    Revealing "false negative" of MultilineLambdaItParameter

--- src/main/kotlin/pck/MyFile.kt
+++ src/main/kotlin/pck/MyFile.kt
@@ -1,12 +1,14 @@
 package pck
 
 fun foo() {
+    // MultilineLambdaItParameter should be reported
     val digits = 1234.let {
         println(it)
         listOf(it)
     }
     println(digits)
 
-    for (i in 0..1)
-        println(i)
+    // will break the build as expected, when uncommented
+//    for (i in 0..1)
+//        println(i)
 }
