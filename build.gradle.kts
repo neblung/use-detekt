c78755f62fb5ce9da4826afbcddeeb1a690bf50c
Frank Neblung <frank.neblung@gmail.com>
2022-04-26 15:27:36 +0200

    Configure detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.20"
+    id("io.gitlab.arturbosch.detekt").version("1.20.0")
 }
 
 group = "com.github.neblung"
@@ -15,10 +16,25 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
+detekt {
+    toolVersion = "1.20.0"
+    config = files("config/detekt/detekt.yml")
+    buildUponDefaultConfig = true
+}
+
 tasks.test {
     useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        txt.required.set(true)
+        sarif.required.set(true)
+    }
 }
